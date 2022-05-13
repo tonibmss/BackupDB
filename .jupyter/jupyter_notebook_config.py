14721defdeb9ce7b94823b287ceba169ed1c18d5
tonibmss <86227728+tonibmss@users.noreply.github.com>
2022-05-13 12:59:53 +0300

    Delete .jupyter directory

--- .jupyter/jupyter_notebook_config.py
+++
@@ -1,4 +0,0 @@
-## Supply overrides for terminado. Currently only supports "shell_command".
-c.NotebookApp.terminado_settings = {
-    'shell_command': ['/usr/bin/env', 'TERM=xterm-256color', '/bin/bash'],
-}
